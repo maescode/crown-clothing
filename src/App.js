d74ad8944b3bc4cfa8d22efa5771586cd9886135
maescode <olagokeabel@gmail.com>
2021-12-28 15:32:54 +0100

    We have broken our homepage into componentand finished styling the menu item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import { HomePage } from './homepage.component';
+// import logo from './logo.svg';
+import { HomePage } from './pages/homepage/homepage.component';
 import './App.css';
 
 function App() {
