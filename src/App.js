6f04e13886dfa914b8bf64242236dc4beb9dde92
maescode <olagokeabel@gmail.com>
2021-12-29 16:49:59 +0100

    add header link for home, contact and hopthen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import { HomePage } from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
         <Route exact={true} path='/shop' component={ShopPage} />
